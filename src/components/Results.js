172dbafee88b3524db581d7de62cc5a1e99e4900
lehoanganh298 <lehoanganh29896@gmail.com>
2020-07-01 17:33:19 +0700

    Result page: add remove button, add filename

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,61 +1,56 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
 import { saveAs } from 'file-saver';
-import {
-	GridContextProvider,
-	GridDropZone,
-	GridItem,
-	swap
-} from "react-grid-dnd";
-// import InfiniteScroll from 'react-bidirectional-infinite-scroll';
-// import './carousel.css'
-// import {maxImageSize} from './ImageGrid';
-const maxImageSize = 230;
-const numAdjacentImages = 52;
+import { GridContextProvider, GridDropZone, GridItem, swap } from "react-grid-dnd";
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
+import update from 'immutability-helper';
+
+const boxesPerRow = 4;
+const rowHeight = 300
 
 const useStyles = makeStyles(theme => ({
-	gridList: {
-		display: 'flex',
-		flexWrap: 'wrap',
-		justifyContent: 'center',
-		alignItems: 'center',
-		overflow: 'hidden',
-		backgroundColor: theme.palette.background.paper,
+	gridItem: {
+		padding: 10,
+	},
+	imageContainer: {
+		width: "100%",
+		height: "100%",
+		display: 'relative',
+		textAlign: 'center'
 	},
 	image: {
-		display: 'block',
-		maxHeight: maxImageSize,
-		maxWidth: maxImageSize,
-		width: 'auto',
-		height: 'auto'
-	},
-	imgFullWidth: {
-		// top: '50%',
-		// width: '100%',
-		// position: 'relative',
-		transform: 'translateY(0%)',
-	},
-	dialog: {
-		minHeight: '80vh',
-		maxHeight: '80vh',
-	},
-	queryImage: {
-		border: 'solid red 2px',
-		display: 'block',
-		maxHeight: 270,
-		maxWidth: 270,
-		width: 'auto',
+		width: '100%',
 		height: 'auto',
-		// boxShadow: '15px 21px 44px 3px rgba(0,0,0,0.57)',
+		margin: 0
+	},
+	card: {
+		width: '100%'
+	},
+	content:{ 
+		display: 'flex', 
+		padding: '0px 4px 0px 4px', 
+		marginTop: '-8px' ,
+		'&:last-child':{
+			paddingBottom:0,
+		}
+	},
+	filename: {
+		wordBreak: 'break-all',
+		overflowWrap: 'break-word',
+		padding: '8px 8px 4px 8px'
+	},
+	icon:{
+		 margin: '4px 0px' 
 	}
 }))
 
@@ -68,14 +63,16 @@ export default function AdjacentImages(props) {
 		saveAs(blob, "results.txt");
 	}
 
+	const handleRemove = (index) => {
+		props.setResults(update(props.results,{
+			$splice:[[index,1]]
+		}))
+	}
 	// target id will only be set if dragging from one dropzone to another.
 	function onChange(sourceId, sourceIndex, targetIndex, targetId) {
 		const changedResults = swap(props.results, sourceIndex, targetIndex);
 		props.setResults(changedResults);
 	}
-
-	const boxesPerRow = 4;
-	const rowHeight = 200
 
 	return (
 		<div>
@@ -84,7 +81,7 @@ export default function AdjacentImages(props) {
 				aria-labelledby="scroll-dialog-title"
 				aria-describedby="scroll-dialog-description"
 				onClose={props.handleCloseResults}
-        disableScrollLock={ true }
+				disableScrollLock={true}
 				fullWidth={true}
 				maxWidth="lg"
 			>
@@ -101,41 +98,31 @@ export default function AdjacentImages(props) {
 								rowHeight={rowHeight}
 								style={{ height: Math.ceil(props.results.length / boxesPerRow) * rowHeight }}
 							>
-								{props.results.map((item, index) => (
-									<GridItem key={item} style={{
-										// width: "100%",
-										// height: "100%",
-										padding: 10
-									}}>
-										<div
-											style={{
-												width: "100%",
-												height: "100%", display: 'relative', textAlign: 'center'
-											}}
-										>
-											<img src={`/LSC_Thumbnail/${item}`} onDragStart={(e) => { e.preventDefault(); }}
-												style={{ width: 'auto', height: 'auto', maxHeight: '100%', maxWidth: '100%' }} />
+								{props.results.map((image, index) => (
+									<GridItem key={image} className={classes.gridItem}>
+										<Card className={classes.card}>
+											<img src={`/LSC_Thumbnail/${image}`} className={classes.image}
+												onDragStart={(e) => { e.preventDefault(); }} />
 
-										</div>
+											<CardContent className={classes.content}>
+												<Typography variant="body2" color="textSecondary" component="p" className={classes.filename}>
+													{image}
+												</Typography>
+												
+												<IconButton className={classes.icon} onClick={()=>{handleRemove(index)}}>
+													<DeleteIcon />
+												</IconButton>
+											</CardContent>
+
+										</Card>
 									</GridItem>
 								))}
 							</GridDropZone>
 						</GridContextProvider>
-						{/* <GridList cellHeight={'auto'} cols={4} spacing={6} classes={{ root: classes.gridList }}>
-
-								{props.results.map((image, index) => (
-									<GridListTile key={image} 
-									>
-										<img src={`/LSC_Thumbnail/${image}`}/>
-									</GridListTile>
-
-								))}
-						</GridList> */}
 					</DialogContentText>
 				</DialogContent>
+
 				<DialogActions>
-
-
 					<Button onClick={() => { exportToFile(props.results) }} color="primary">
 						Export to File
           </Button>
