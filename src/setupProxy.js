f9867205228bd256ca91bc0a19bb1bc38a72b320
lehoanganh298 <lehoanganh29896@gmail.com>
2020-07-04 20:00:32 +0700

    Change default server address to localhost

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
   app.use(
     '/server',
     createProxyMiddleware({
-      target: 'http://'+process.env.REACT_APP_SERVER_ADDRESS || 'http://0.0.0.0:8000',
+      target: process.env.REACT_APP_SERVER_ADDRESS ? 'http://'+process.env.REACT_APP_SERVER_ADDRESS : 'http://127.0.0.1:8000',
       changeOrigin: true,
     })
   );
